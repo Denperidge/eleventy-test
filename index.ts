f598302af37773fafdd6b3c837a76d5efc8b1f79
Denperidge <cat@denperidge.com>
2024-12-05 21:04:30 +0100

    wip

--- index.ts
+++ index.ts
@@ -1,25 +1,30 @@
 import {cwd} from "process";
 import {join} from "path";
-import { readFileSync, rmSync } from "fs";
-import {exec} from "child_process";
-
+import { readFileSync, rmSync, readdirSync } from "fs";
+import {execSync, fork} from "child_process";
 const DIR_BASE = "tests";
 const DIR_SCENARIOS = join(DIR_BASE, "scenarios");
 const DIR_TESTS = join(DIR_BASE, "tests");
 
-function buildEleventy(eleventyVersion, scenarioDir, hashTruncate=16, runAsync=true, useServe=false) {
+function buildEleventy(eleventyVersion, scenarioDir, projectRoot=cwd(), hashTruncate=16, runAsync=true, useServe=false) {
 	// I tried using Eleventy programmatically. Emphasis on tried
 	// Thanks to https://github.com/actions/setup-node/issues/224#issuecomment-943531791
 
-    const eleventyDir=`node_modules/@11ty/${eleventyVersion}/`;
-    //const bin = JSON.parse()
-    const bin = JSON.parse(readFileSync(join(eleventyDir, "package.json"), {encoding: "utf-8"})).bin
-    console.log(bin)
-    return;
+    const eleventyDir=join(projectRoot, "node_modules/@11ty/", eleventyVersion);
+    const bin = JSON.parse(
+        readFileSync(
+            join(eleventyDir, "package.json"),
+            {encoding: "utf-8"})
+        ).bin.eleventy;
+    const pathToBin = join(eleventyDir, bin);
+    console.log(pathToBin)
 
 	try {
-		const command = useServe ? "timeout 2 npx @11ty/eleventy --serve" : "npx @11ty/eleventy"
-		child_process.execSync(command, {cwd: scenarioDir});
+		const command = useServe ? `timeout 5 node ${pathToBin} --serve` : `node ${pathToBin}`
+		const out = fork(pathToBin, {cwd: scenarioDir}).stdout
+        
+        //const out = execFile("node", ["--version"], {cwd: scenarioDir, encoding: "utf-8"});
+        console.log(out)
 	} catch (e) {
 		if (!useServe) {
 			throw e;
@@ -30,8 +35,13 @@ function buildEleventy(eleventyVersion, scenarioDir, hashTruncate=16, runAsync=t
 
 
 function main(directory=cwd()) {
-    console.log("AAA")
-    buildEleventy("eleventy3", "")
+    const scenarios = readdirSync(DIR_SCENARIOS);
+    scenarios.forEach(dirname => {
+        console.log(dirname)
+        const scenarioDir = join(DIR_SCENARIOS, dirname)
+        buildEleventy("eleventy3", scenarioDir, cwd())
+    })
+    return;
     buildEleventy("eleventy2", "")
     buildEleventy("eleventy1", "")
 }
